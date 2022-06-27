98427307678222a5d51cdebb55ffe3473830d548
zhaol <zhao@123.com>
2022-06-27 20:39:57 +0800

    hot-fix test

--- src/main/java/com/example/demo/git/GitTest.java
+++ src/main/java/com/example/demo/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hhhhhhhahdh");
         System.out.println("我要打十个");
+        System.out.println("hotfix");
     }
 }
